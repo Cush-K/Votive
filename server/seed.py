e9e0fd636d45bbc4ccac3c2207ded9dab1d05db1
Kalvine <kkalvine4@gmail.com>
2024-12-03 19:16:43 +0300

    Seeds the database

--- server/seed.py
+++ server/seed.py
@@ -1,26 +1,18 @@
-from models import db, Customer, ScheduledJob, Admin, Message, Image
+from models import db, Admin
 from app import app, bcrypt
-from flask import session
-from faker import Faker
-import random
-from datetime import datetime, date, time
-
-
-fake = Faker()
+import os
 
 with app.app_context():
     
     print('Clearing Database...')
     Admin.query.delete()
-    Message.query.delete()
-    Image.query.delete()
     
 
     print('Seeding Admin Data...')
     admin = Admin(
         username="Admin",
         email= "votiveent@gmail.com",
-        password=bcrypt.generate_password_hash('&VotiVe2021').decode('utf-8')
+        password=bcrypt.generate_password_hash(os.getenv('PASSWORD')).decode('utf-8')
     )
     db.session.add(admin)
     db.session.commit()
@@ -33,18 +25,6 @@ with app.app_context():
     db.session.add(dev)
     db.session.commit()
     
-    print('Generating Image Data...')
-    images = []
-    links = ["/", "/about-votive-laundry", "/contact-votive-laundry"]
-    for _ in range(10):
-        image = Image(
-            name=fake.word(),
-            image_url="https://img.freepik.com/premium-photo/black-afro-woman-smiling-cheerfully-feeling-happy-showing-concept-copy-space-with-palm-hand-housekeeping-concept-household-concept_1194-212717.jpg?w=740",
-            redirect_link=random.choice(links)
-        )
-        images.append(image)
-    db.session.add_all(images)
-    db.session.commit() 
-    
+       
     print("Seeding Complete!")
             
